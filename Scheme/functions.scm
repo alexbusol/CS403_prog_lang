f9bd20ff16b8e1fd50bbc13e10dc3d3f7b24b130
Alex Busol <ovbusol@crimson.ua.edu>
2018-12-03 22:55:42 -0600

    Added more recursive functions

--- Scheme/functions.scm
+++ Scheme/functions.scm
@@ -35,3 +35,29 @@
             (+ (countElements (car list)) (countElements (cdr list))))))
 
 
+;filter elements based on some test 
+
+(define (filter test list) 
+    (if (null? list) '()
+        (if (test (car list)) (cons (car list) (myfilter test (cdr list))) 
+            (myfilter test (cdr list)))))
+
+
+;returns true if the element is a part of the list
+(define (detect element list) (if (null? list) #f 
+    (if (eq? element (car list)) #t 
+        (detect element (cdr list)))))
+
+
+;appends the second list to the first
+
+(define (append list1 list2) (if (null? list1) list2 
+    (cons (car list1) (append (cdr list1) list2))))
+
+;reverses the list
+
+(define (reverse L) 
+    (if (null? L) '()
+    (append (reverse (cdr L)) (list (car L)))))
+
+
