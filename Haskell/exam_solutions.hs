6c9bbee006a392140a1ac5b72dbd03ce17cb8353
Alex Busol <ovbusol@crimson.ua.edu>
2018-12-07 20:42:15 -0600

    Added more solved exercises

--- Haskell/exam_solutions.hs
+++ Haskell/exam_solutions.hs
@@ -16,3 +16,29 @@ extremes ((x:xs):ys) = [x, last xs] : extremes ys
 
 curry3 f x y z = f (x,y,z)
 uncurry3 f (x,y,z) = f x y z
+
+count _ [] = 0
+count e (x:xs) = if x==e then 1 + count e xs else count e xs
+
+partition p list = ([x | x<-list, p x], [x | x<-list, not (p x)])
+
+isPerfect n = foldr (+) 0 [x | x<-[1..n-1], mod n x == 0] == n
+
+rotations list = map (\k -> drop k list ++ take k list) [0..length list - 1]
+
+transpose ([]:_) = []
+transpose m = map head m : transpose (map tail m)
+
+intersect [] [] = []
+intersect (x:xs) (y:ys) = if x == y then x : intersect xs ys else intersect xs ys
+
+combine f g h xs ys = zipWith f (map g xs) (map h ys)
+                                                                             
+apply3 (f,g,s) (x,y,z) = (f x, g y, s z)
+
+series _ _ 0 = []
+series s f n = s : series (f s) f (n-1)
+
+powerset [] = [[]]
+powerset (x:xs) = (map (x:) s) ++ s where s = powerset xs
+
