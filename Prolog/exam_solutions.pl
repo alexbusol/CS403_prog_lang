448153ee6e6238dd515a7ca66aa6be9c81b65424
Alex Busol <ovbusol@crimson.ua.edu>
2018-12-08 23:07:19 -0600

    Solving previous exam problems

---
+++ Prolog/exam_solutions.pl
@@ -0,0 +1,37 @@
+inner([], _, 0).
+inner([H1|T1], [H2|T2], R) :- inner(T1, T2, A), R is (H1*H2) + A.
+
+outer([], _, []).
+outer([H|T], L2, [HR|TR]) :- ohelper(H, L2, HR), outer(T, L2, TR).
+ohelper(_, [], []).
+ohelper(H, [H2|T], [HR|TR]) :- HR is H*H2, ohelper(H, T, TR).
+
+scan([], []).
+scan(L, R) :- scanhelp(0, L, R). 
+scanhelp(N, [], [N|[]]).
+scanhelp(N, [H|T], [N|TR]) :- M is N+H, scanhelp(M, T, TR).
+
+not(A) :- \+A.
+
+prime(1).
+prime(N) :- primecheck(N, 2). 
+primecheck(N, N). 
+primecheck(N, K) :- K<N, not(0 is mod(N, K)), J is K+1, primecheck(N, J). 
+
+
+selection_sort([], []).
+selection_sort(L, [H|T]) :- minimum(L, H), remove(H, L, X), selection_sort(X, T). 
+
+minimum([X], X). 
+minimum([X, Y|Z], M) :- X<Y, minimum([X|Z], M). 
+minimum([X, Y|Z], M) :- X>=Y, minimum([Y|Z], M). 
+
+remove(X, [H|T], [H|TR]) :- remove(X, T, TR).
+remove(X, [X|T], T). 
+
+insertion_sort(L, R) :- ihelper(L, [], R).
+ihelper([], A, A).
+ihelper([H|T], A, R) :- insert(H, A, Q), ihelper(T, Q, R).
+insert(X, [Y|T], [Y|TR]) :- X>Y, insert(X, T, TR).
+insert(X, [Y|T], [X,Y|T]) :- X=<Y.
+insert(X, [], [X]).
